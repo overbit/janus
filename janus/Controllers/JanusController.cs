380a80fa7dda2fc7c6e802a0f04509147663e35f
overbit <2861984+overbit@users.noreply.github.com>
2019-12-13 18:13:30 +0000

    Add .net Core solution and project

---
+++ janus/Controllers/JanusController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace overapp.janus.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JanusController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        [Route("")]
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult UnAuthorised()
+        {
+            re
+        }
+    }
+}
---
+++ janus/Models/Dtos/Request/Transaction.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace overapp.janus.Models.Dtos.Request
+{
+    public class Transaction
+    {
+        public Transaction()
+        {
+        }
+    }
+}
