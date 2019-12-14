0a79edd316a78043113d9d09678edfe0d8ad7137
overbit <2861984+overbit@users.noreply.github.com>
2019-12-14 09:46:43 +0000

    Add transaction model

---
+++ janus/Models/Domain/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace overapp.janus.Models.Domain
+{
+    public class Transaction
+    {
+        public string Id { get; set; }
+
+        public string BankTransactionId { get; set; }
+
+        public double Amount { get; set; }
+
+        public RegionInfo BillingRegion { get; set; }
+
+        public Card CardDetails { get; set; }
+
+        public BillingDetails BillingDetails { get; set; }
+    }
+}
