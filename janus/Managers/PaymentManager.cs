cebfb8a6ec4927bac14976715aca93b7ed77e642
overbit <2861984+overbit@users.noreply.github.com>
2019-12-20 11:36:52 +0000

    Add manager test

--- janus/Managers/PaymentManager.cs
+++ janus/Managers/PaymentManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using overapp.janus.Infrastructure.Repositories;
+using overapp.janus.Infrastructure.Services;
 using overapp.janus.Models.Dtos.Request;
 using overapp.janus.Models.Dtos.Response;
 
@@ -8,6 +10,17 @@ namespace overapp.janus.Managers
 {
     public class PaymentManager : IPaymentManager
     {
+        private readonly IPaymentRepository paymentRepository;
+        private readonly IMerchantRepository merchantRepository;
+        private readonly IBankService bankService;
+
+        public PaymentManager(IPaymentRepository paymentRepository, IMerchantRepository merchantRepository, IBankService bankService)
+        {
+            this.paymentRepository = paymentRepository;
+            this.merchantRepository = merchantRepository;
+            this.bankService = bankService;
+        }
+
         public async Task<TransactionDetails> GetPaymentDetails(string clientId, Guid paymentGuid)
         {
             throw new NotImplementedException();
--- janusTests/Managers/PaymentManagerTests.cs
+++ janusTests/Managers/PaymentManagerTests.cs
@@ -1,36 +1,80 @@
-﻿using NUnit.Framework;
-using overapp.janus.Managers;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using overapp.janus.Infrastructure.Repositories;
+using overapp.janus.Infrastructure.Services;
+using overapp.janus.Models.Dtos.Request;
 
 namespace overapp.janus.Managers.Tests
 {
-    [TestFixture()]
+    [TestFixture]
     public class PaymentManagerTests
     {
-        [Test()]
+        private Mock<IPaymentRepository> paymentRepoMock; 
+        private Mock<IMerchantRepository> merchantRepoMock; 
+        private Mock<IBankService> bankServiceMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            paymentRepoMock = new Mock<IPaymentRepository>(MockBehavior.Strict);
+            merchantRepoMock = new Mock<IMerchantRepository>(MockBehavior.Strict);
+            bankServiceMock = new Mock<IBankService>(MockBehavior.Strict);
+        }
+
+        [Test]
         public void GetPaymentDetailsTest()
         {
             Assert.Fail();
         }
 
-        [Test()]
-        public void GetPaymentsPerMerchantTest()
-        {
-            Assert.Fail();
-        }
+        //[Test()]
+        //public void GetPaymentsPerMerchantTest()
+        //{
+        //    Assert.Fail();
+        //}
 
-        [Test()]
-        public void GetPaymentsPerMerchantByDateTest()
-        {
-            Assert.Fail();
-        }
+        //[Test()]
+        //public void GetPaymentsPerMerchantByDateTest()
+        //{
+        //    Assert.Fail();
+        //}
 
-        [Test()]
-        public void ProcessPaymentTest()
+        [Test]
+        public async Task ProcessPaymentTest()
         {
-            Assert.Fail();
+            // Arrange 
+            var clientId = "123";
+            var req = new TransactionRequest()
+            {
+                Amount = 1005.3,
+                CurrencyCode = "EUR",
+                BillingDetails = new BillingDetails
+                {
+                    FirstName = "Massimo",
+                    LastName = "Bottura",
+                    AddressLine1 = "Osteria Francescana",
+                    AddressLine2 = "Via Stella, 22",
+                    City = "Modena",
+                    Country = "Italy"
+                },
+                Card = new Card
+                {
+                    Number = "4111111111111111",
+                    Cvv = "123",
+                    ExpiryMonth = "11",
+                    ExpiryYear = "25"
+                }
+            };
+
+            var manager = new PaymentManager(paymentRepoMock.Object, merchantRepoMock.Object, bankServiceMock.Object);
+
+            // Act 
+            var result = await manager.ProcessPayment(clientId, req);
+
+            // Assert
+            Assert.IsNotNull(result.Guid);
+            Assert.IsTrue(result.IsSuccess);
         }
     }
 }
