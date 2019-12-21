e2b037055876e874d0253604c5abd07c17545215
overbit <2861984+overbit@users.noreply.github.com>
2019-12-21 15:43:06 +0000

    Add dummy data in db for testing purposes at program startup

---
+++ janus/DataGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using overapp.janus.Infrastructure;
+using overapp.janus.Models.Domain;
+
+namespace overapp.janus
+{
+    /// <summary>
+    /// Class to auto generate data in inMemory DB. NOT FOR PROD
+    /// </summary>
+    public static class DataGenerator
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new JanusContext(serviceProvider.GetRequiredService<DbContextOptions<JanusContext>>()))
+            {
+                if (context.Merchants.Any())
+                {
+                    // Data is already present
+                    return;
+                }
+
+                // Add fake merchants
+                context.Merchants.Add(new Merchant
+                {
+                    ClientId = "123456",
+                    ClientSecret = "psss-it-s-a-secret",
+                    Id = 1,
+                    Name = "Merchant1"
+                });
+
+
+                context.Merchants.Add(new Merchant
+                {
+                    ClientId = "1",
+                    ClientSecret = "psss-it-s-a-secret2",
+                    Id = 2,
+                    Name = "Merchant2"
+                });
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- janus/Program.cs
+++ janus/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using overapp.janus.Infrastructure;
 
 namespace overapp.janus
 {
@@ -7,7 +9,16 @@ namespace overapp.janus
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<JanusContext>();
+                DataGenerator.Initialize(services);
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
