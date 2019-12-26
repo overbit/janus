1ff1bdc3c4753a0e5b6d18b318fa12ecf5d45207
overbit <2861984+overbit@users.noreply.github.com>
2019-12-26 17:19:10 +0100

    Add unit tests for controller

--- janusTests/Controllers/JanusControllerTests.cs
+++ janusTests/Controllers/JanusControllerTests.cs
@@ -1,24 +1,74 @@
-﻿using NUnit.Framework;
-using overapp.janus.Controllers;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using overapp.janus.Managers;
+using overapp.janus.Models.Dtos.Request;
+using overapp.janus.Models.Dtos.Response;
 
 namespace overapp.janus.Controllers.Tests
 {
-    [TestFixture()]
+    [TestFixture]
     public class JanusControllerTests
     {
-        [Test()]
-        public void GetTransactionTest()
+        Mock<IPaymentManager> paymentManager;
+
+        [SetUp]
+        public void Setup()
         {
-            Assert.Fail();
+            paymentManager = new Mock<IPaymentManager>(MockBehavior.Strict);
         }
 
-        [Test()]
-        public void ProcessTransactionTest()
+        [Test]
+        public async Task GetTransactionReturns400WhenInvalidIdTest()
         {
-            Assert.Fail();
+            var paymentId = "invalidguid";
+            var controller = new JanusController(paymentManager.Object);
+
+            var result = await controller.GetTransaction("123", null, paymentId);
+
+            Assert.AreEqual(typeof(BadRequestResult), result.Result.GetType());
+        }
+
+        [Test]
+        public async Task GetTransactionReturns404WhenNoTransactionIsFoundTest()
+        {
+            var paymentId = Guid.NewGuid().ToString("N");
+            var controller = new JanusController(paymentManager.Object);
+
+            paymentManager.Setup(manager => manager.GetPaymentDetails("123", paymentId)).ReturnsAsync((TransactionDto)null);
+
+            var result = await controller.GetTransaction("123", null, paymentId);
+
+            Assert.AreEqual(typeof(NotFoundResult), result.Result.GetType());
+        }
+
+        [Test]
+        public async Task GetTransactionTest()
+        {
+            var paymentId = Guid.NewGuid().ToString("N");
+            var controller = new JanusController(paymentManager.Object);
+
+            paymentManager.Setup(manager => manager.GetPaymentDetails("123", paymentId)).ReturnsAsync(new TransactionDto
+            {
+                Id = paymentId
+            });
+
+            var result = await controller.GetTransaction("123", null, paymentId);
+
+            Assert.AreEqual(paymentId, result.Value.Id);
+        }
+
+        [Test]
+        public async Task ProcessTransactionTest()
+        {
+            paymentManager.Setup(manager => manager.ProcessPayment("123", It.IsAny<TransactionRequest>())).ReturnsAsync(new TransactionResultDto());
+            var controller = new JanusController(paymentManager.Object);
+
+            var result = await controller.ProcessTransaction("123", null, new TransactionRequest());
+
+            Assert.IsNotNull(result.Value);
         }
     }
 }
